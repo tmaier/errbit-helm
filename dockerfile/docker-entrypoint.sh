af7a0aa7cb4b629248d2e0ea2f362f67f0dd050d
Tobias L. Maier <me@tobiasmaier.info>
2017-12-20 22:41:51 +0100

    Fix wrong env variable used in entrypoint

--- dockerfile/docker-entrypoint.sh
+++ dockerfile/docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [ -z "$MONGO_URL" ]; then
   export MONGO_URL="mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MONGODB_PORT:-27017}/${MONGODB_DATABASE}"
 fi
 
-if [ "$MONGO_URL" ]; then
+if [ "$MONGODB_HOST" ]; then
   echo "Waiting for MongoDB to start..."
   while ! nc -z "${MONGODB_HOST}" "${MONGODB_PORT:-27017}"; do sleep 0.1; done
   echo "MongoDB is up"
