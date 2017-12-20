67bc232d894dff52b730d8e073d09cf0afc62307
Tobias L. Maier <me@tobiasmaier.info>
2017-12-20 19:01:59 +0100

    Add docker-entrypoint.sh
    Overrides the MONGO_URL based on MONGODB_ env variables

---
+++ configs/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$MONGO_URL" ]; then
+  export MONGO_URL="mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MONGODB_PORT:-27017}/${MONGODB_DATABASE}"
+fi
+
+if [ "$MONGO_URL" ]; then
+  echo "Waiting for MongoDB to start..."
+  while ! nc -z "${MONGODB_HOST}" "${MONGODB_PORT:-27017}"; do sleep 0.1; done
+  echo "MongoDB is up"
+fi
+
+exec bundle exec "$@"
